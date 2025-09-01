16cd045d9bf3494d11a58b72c892d4a628bc2398
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-09-01 16:54:06 +0530

    cors fixed

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { items } from "./data.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+
+
+// Allow all origins (dev mode)
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Shopping API is running...");
