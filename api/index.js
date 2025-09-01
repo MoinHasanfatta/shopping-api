8c2245d001ef8d9f2b65ecdaf6d63929704ae666
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-09-01 17:47:27 +0530

    api

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { items } from "./data.js";
+import { items } from "../data.js";
 
 const app = express();
-
-// Middleware
 app.use(cors());
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Shopping API is running...");
 });
@@ -16,5 +13,4 @@ app.get("/shopping", (req, res) => {
   res.json(items);
 });
 
-// Export for Vercel
 export default app;
