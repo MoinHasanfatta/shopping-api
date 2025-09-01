6a81c9434a97bc6219bd863bef8b03854062e19d
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-09-01 16:44:03 +0530

    shopping api

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { items } from "./data.js";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Shopping API is running...");
+});
+
+app.get("/shopping", (req, res) => {
+  res.json(items);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
